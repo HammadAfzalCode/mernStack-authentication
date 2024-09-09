f407145c9ea7b88ea40bf39cbac63beb9ca567a5
Hammad Afzal <hammad.afzal.code@gmail.com>
2024-09-09 15:50:50 +0500

    added signup controller || mailtrap setup && sending emails

--- mern-authentication-backend/controllers/auth.controller.js
+++ mern-authentication-backend/controllers/auth.controller.js
@@ -1,6 +1,49 @@
+import { User } from "../models/user.model.js"
+import bcryptjs from 'bcrypt'
+import { generateVerificationCode } from "../utils/generateVerificationCode.js"
+import { generateTokenAndSetCookie } from "../utils/generateTokenAndSetCookie.js"
+import { sendVerificationEmail } from "../mailtrap/emails.js"
 
-export const signup = (req, res) => {
-    res.send("Auth routes")
+export const signup = async (req, res) => {
+    const { email, password, name } = req.body
+    try {
+        if (!email || !password || !name) {
+            throw new Error("All fields are required")
+        }
+        const userAlreadyExits = await User.findOne({ email });
+        if (userAlreadyExits) {
+            return res.status(400).json({
+                success: false,
+                message: "User already exists"
+            })
+        }
+        const hashedPassword = await bcryptjs.hash(password, 10)
+        const verificationToken = generateVerificationCode()
+        const user = new User({
+            email,
+            password: hashedPassword,
+            name,
+            verificationToken,
+            verificationTokenExpiresAt: Date.now() + 24 * 60 * 60 * 1000
+        })
+        await user.save();
+
+        generateTokenAndSetCookie(res, user._id);
+        sendVerificationEmail(user.email, verificationToken)
+        res.status(201).json({
+            success: true,
+            message: "User created successfully",
+            user: {
+                ...user._doc,
+                password: undefined
+            }
+        })
+    } catch (error) {
+        res.status(400).json({
+            success: false,
+            message: error.message
+        })
+    }
 }
 
 export const login = (req, res) => {
